c1ae81b906c38104b5c1cd2d610afdb9a0bdbd37
rabbitmask <rabbitmask@163.com>
2025-11-19 14:08:37 +0800

    BeforeDawn - Docker一键部署

--- frontend/assets/permission-C3dSjPsm.js
+++
@@ -1,1 +0,0 @@
-import{b as o}from"./index-DNLodC6g.js";function u(r){if(!Array.isArray(r))return console.error("hasAnyRole() 参数必须是数组"),!1;const e=o().roles||[];return r.some(s=>e.includes(s))}export{u as h};
