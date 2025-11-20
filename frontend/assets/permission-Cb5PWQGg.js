826a163b17269893ec9117a2858787be5b94d029
rabbitmask <rabbitmask@163.com>
2025-11-20 17:12:15 +0800

    BeforeDawn - Docker一键部署

---
+++ frontend/assets/permission-Cb5PWQGg.js
@@ -0,0 +1,1 @@
+import{b as o}from"./index-gZy6T0gL.js";function u(r){if(!Array.isArray(r))return console.error("hasAnyRole() 参数必须是数组"),!1;const e=o().roles||[];return r.some(s=>e.includes(s))}export{u as h};
