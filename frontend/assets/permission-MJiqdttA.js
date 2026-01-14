d84f2607a9315555d73bbfd50e4a67bd2fa5a2f2
rabbitmask <rabbitmask@163.com>
2026-01-14 10:41:05 +0800

    BeforeDawn - Docker一键部署

---
+++ frontend/assets/permission-MJiqdttA.js
@@ -0,0 +1,1 @@
+import{u as o}from"./index-P-kv1zA8.js";function u(r){if(!Array.isArray(r))return console.error("hasAnyRole() 参数必须是数组"),!1;const e=o().roles||[];return r.some(s=>e.includes(s))}export{u as h};
