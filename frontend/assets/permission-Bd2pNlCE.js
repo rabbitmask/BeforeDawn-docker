3484256808a5fd5f9bc1b089da4c0f9bf6860f64
rabbitmask <rabbitmask@163.com>
2026-02-24 14:39:40 +0800

    BeforeDawn - Docker一键部署

---
+++ frontend/assets/permission-Bd2pNlCE.js
@@ -0,0 +1,1 @@
+import{u as o}from"./index-D-KiCPd7.js";function u(r){if(!Array.isArray(r))return console.error("hasAnyRole() 参数必须是数组"),!1;const e=o().roles||[];return r.some(s=>e.includes(s))}export{u as h};
--- frontend/assets/permission-CqqDIsbt.js
+++
@@ -1,1 +0,0 @@
-import{u as o}from"./index-DG9m7imz.js";function u(r){if(!Array.isArray(r))return console.error("hasAnyRole() 参数必须是数组"),!1;const e=o().roles||[];return r.some(s=>e.includes(s))}export{u as h};
